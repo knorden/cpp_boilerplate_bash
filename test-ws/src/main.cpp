346aa388631be1b50c901b1d5822776056a28300
Karl Norden <knorden@github.com>
2020-10-04 23:15:37 -0700

    removed test creation

--- test-ws/src/main.cpp
+++
@@ -1,7 +0,0 @@
-#include "unitTest.h"
-
-int main() {
-	// call the unit test function
-	unitTestFunc();
-	return 0;
-}
--- test-ws/src/unitTest.cpp
+++
@@ -1,8 +0,0 @@
-#include "unitTest.h"
-
-void unitTestFunc() {
-	std::string tString = "THIS IS A TEST STRING.";
-	std::cout << tString << std::endl;
-
-	return;
-}
